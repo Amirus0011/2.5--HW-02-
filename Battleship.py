66bdd9309ce042b10c39b9f2a7b07f6820de9e30
Amirus0011 <110290725+Amirus0011@users.noreply.github.com>
2022-08-12 00:35:25 +0500

    Add files via upload

---
+++ Battleship.py
@@ -0,0 +1,26 @@
+ class Board:
+     def  init  (self, hid=False, size=6):
+         self.size = size
+         self.hid = hid
+ 
+         self.count = 0
+ 
+         self.field = [["O"] * size for _ in range(size)]
+ 
+         self.busy = []
+         self.ships = []
+ 
+     def str(self):
+         res = ""
+         res += "  | 1 | 2 | 3 | 4 | 5 | 6 |"
+         for i, row in enumerate(self.field):
+             res += f"\n{i + 1} | " + " | ".join(row) + " |"
+ 
+         if self.hid:
+             res = res.replace("■", "O")
+         return res
+ 
+ 
+ 
+ b = Board()
+ print(b)
